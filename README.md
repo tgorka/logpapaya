a69c3cd499c41f88a2c924bf4231ec0f3bb9ac6a
noam1610 <noam.benlolo@getcarmoov.com>
2017-09-28 16:34:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#logPapaya
+
+This module is responsible for all the system logging.
+It has two goals
---
+++ lib/logging.ts
@@ -0,0 +1,61 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const winston = require("winston");
+const logdna = require("logdna");
+const options = {
+    app: "default-app",
+    env: "default-env",
+    index_meta: true,
+    handleExceptions: true,
+};
+const defaultLevels = { error: 0, warn: 1, info: 2, verbose: 3, debug: 4, silly: 5 };
+const prodLevels = { error: 0, warn: 1, info: 2 };
+
+class Logging {
+
+    /**
+     * constructor. Create and configure logging params
+     *
+     * @constructor
+     */
+    constructor() {
+    }
+
+    public init(config: any, environnement: string) {
+        console.log(Object.assign({}, options, config, { hostname: environnement }))
+        winston.add(logdna.WinstonTransport, Object.assign({}, options, config, { hostname: environnement }));
+        if (environnement == "production") {
+            winston.setLevels(prodLevels)
+        } else {
+            winston.setLevels(defaultLevels)
+        }
+    }
+
+    public error(message: any) {
+        winston.level = 'error';
+        winston.log('error', message);
+    }
+    public warn(message: any) {
+        winston.level = 'warn';
+        winston.log('warn', message);
+    }
+    public info(message: any) {
+        winston.level = 'info';
+        winston.log('info', message);
+    }
+    public verbose(message: any) {
+        winston.level = 'verbose';
+        winston.log('verbose', message);
+    }
+    public debug(message: any) {
+        winston.level = 'debug';
+        winston.log('debug', message);
+    }
+    public silly(message: any) {
+        winston.level = 'silly';
+        winston.log('silly', message);
+    }
+
+}
+
+export let logging = new Logging();
